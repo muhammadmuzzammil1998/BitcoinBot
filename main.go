b76f4d71c1fcbf3be51b74d5712a3e7a6fac201d
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-05-04 14:05:54 +0530

    :tada: rewrite :tada:

--- main.go
+++ main.go
@@ -13,6 +13,109 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+//Global Variables
+var codename = "Cherry"
+var version = "2.0"
+var api = "https://api.coinbase.com/v2/prices/spot?currency="
+
+//UpdateStatus updates status on discord
+func UpdateStatus(discord *discordgo.Session) {
+	rate, err := GetPrice("USD")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	discord.UpdateStatus(0, ">btc help | $"+rate)
+}
+
+//GetPrice returns price
+func GetPrice(currency string) (string, error) {
+	resp, err := http.Get(api + currency)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	data := map[string]map[string]string{}
+	json.Unmarshal(body, &data)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	return data["data"]["amount"], nil
+}
+
+//Response for commands
+func Response(s *discordgo.Session, m *discordgo.MessageCreate) {
+	go UpdateStatus(s)
+	message := strings.TrimSpace(m.Content)
+	if strings.HasPrefix(message, ">btc") || strings.HasPrefix(message, "<@388984248062967819>") {
+		curr := "USD"
+		if strings.Contains(message, " ") {
+			if strings.Split(message, " ")[1] == "help" {
+				s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+					Title: "BitcoinBot Help",
+					Color: 0xf4a435,
+					Fields: []*discordgo.MessageEmbedField{
+						CreateField("Usage", ">btc <currency> or @BitcoinBot#9430 <currency>", false),
+						CreateField("Examples", ">btc, >btc USD, @BitcoinBot#9430, @BitcoinBot#9430 usd", false),
+						CreateField("BitcoinBot's BTC Address", "3KyXwJhu1FpaPukJnzG9bPzn46xJ2ggTAs", true),
+						CreateField("Version", codename+" ("+version+")", true),
+						CreateField("Website", "https://bit.ly/btcbot", true),
+					},
+				})
+			}
+			curr = strings.Split(message, " ")[1]
+		}
+		rate, err := GetPrice(curr)
+		if err != nil {
+			log.Println(err)
+			Report(s, m.ChannelID)
+			return
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+			Title:       "Bitcoin Price",
+			Color:       0xf4a435,
+			Description: "Current Bitcoin per " + strings.ToUpper(curr) + " price:",
+			Fields: []*discordgo.MessageEmbedField{
+				CreateField(strings.ToUpper(curr), rate, true),
+			},
+		})
+		/*	For server count management: START	*/
+		f, err := os.OpenFile("/path/to/bitcoinbot/servers", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+			Report(s, m.ChannelID)
+		}
+		f.WriteString(m.ChannelID + "\n")
+		f.Close()
+		/*	For server count management: END	*/
+	}
+}
+
+//Report error
+func Report(s *discordgo.Session, m string) {
+	s.ChannelMessageSendEmbed(m, &discordgo.MessageEmbed{
+		Title:       "Error",
+		Color:       0xe74c3c,
+		Description: "An error occurred. Please report this so I can fix this.",
+		Fields: []*discordgo.MessageEmbedField{
+			CreateField("Report", "https://bit.ly/btcBotReport", false),
+			CreateField("Email", "bitcoinbot@muzzammil.xyz", false),
+		},
+	})
+}
+
+//CreateField creates Message Embed Field and returns its address
+func CreateField(name string, value string, inline bool) *discordgo.MessageEmbedField {
+	return &discordgo.MessageEmbedField{
+		Name:   name,
+		Value:  value,
+		Inline: inline,
+	}
+}
+
 func main() {
 	var token string
 	flag.StringVar(&token, "token", "nil", "Bot token")
@@ -22,95 +125,7 @@ func main() {
 		log.Println(err)
 		return
 	}
-	discord.AddHandler(
-		func(s *discordgo.Session, m *discordgo.MessageCreate) {
-			go func() {
-				resp, err := http.Get("https://api.coinbase.com/v2/prices/spot?currency=USD")
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				defer resp.Body.Close()
-				body, err := ioutil.ReadAll(resp.Body)
-				data := map[string]map[string]string{}
-				json.Unmarshal(body, &data)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				discord.UpdateStatus(0, ">btc help | $"+data["data"]["amount"])
-			}()
-			message := strings.TrimSpace(m.Content)
-			if strings.HasPrefix(message, ">btc") || strings.HasPrefix(message, "<@388984248062967819>") {
-				curr := "USD"
-				if strings.Contains(message, " ") {
-					if strings.Split(message, " ")[1] == "help" {
-						s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-							Title: "BitcoinBot Help",
-							Color: 0xf4a435,
-							Fields: []*discordgo.MessageEmbedField{
-								&discordgo.MessageEmbedField{
-									Name:   "Usage",
-									Value:  ">btc <currency> or @BitcoinBot#9430 <currency>",
-									Inline: false,
-								},
-								&discordgo.MessageEmbedField{
-									Name:   "Examples",
-									Value:  ">btc, >btc USD, @BitcoinBot#9430, @BitcoinBot#9430 usd",
-									Inline: false,
-								},
-								&discordgo.MessageEmbedField{
-									Name:   "BitcoinBot's BTC Address",
-									Value:  "3KyXwJhu1FpaPukJnzG9bPzn46xJ2ggTAs",
-									Inline: true,
-								},
-								&discordgo.MessageEmbedField{
-									Name:   "Version",
-									Value:  "Cherry (2.0)",
-									Inline: true,
-								},
-							}})
-					}
-					curr = strings.Split(message, " ")[1]
-				} else {
-					curr = "USD"
-				}
-				resp, err := http.Get("https://api.coinbase.com/v2/prices/spot?currency=" + curr)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				defer resp.Body.Close()
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				data := map[string]map[string]string{}
-				json.Unmarshal(body, &data)
-
-				s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-					Title:       "Bitcoin Price",
-					Color:       0xf4a435,
-					Description: "Current Bitcoin per " + strings.ToUpper(curr) + " price:",
-					Fields: []*discordgo.MessageEmbedField{
-						&discordgo.MessageEmbedField{
-							Name:   data["data"]["currency"],
-							Value:  data["data"]["amount"],
-							Inline: true,
-						},
-					}})
-				/*	For server count management: START	*/
-				f, err := os.OpenFile("/path/to/bitcoinbot/servers", os.O_APPEND|os.O_WRONLY, 0644)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				f.WriteString(m.ChannelID + "\n")
-				f.Close()
-				/*	For server count management: END	*/
-			}
-		})
+	discord.AddHandler(Response)
 	err = discord.Open()
 	if err != nil {
 		log.Println(err)
